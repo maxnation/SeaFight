4939066d2061f0b28c9eaf4746229f60bb02b2cc
Maxim <maksim.torba@nixsolutions.com>
2020-04-11 19:03:19 +0300

    Added initialization of Context via ContextBase class: Initalization of derived context class properties, which are of type Repository<> (fields representing tables in database)

--- CustomORM/ContextBase.cs
+++ CustomORM/ContextBase.cs
@@ -1,20 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Configuration;
-using System.Data.SqlClient;
+using System.Reflection;
 
 namespace CustomORM
 {
     public abstract class ContextBase
     {
-        protected SqlConnection connection;
+        protected string connectionString;
 
         protected ContextBase(string connectionStringName)
         {
-            string connectionString = ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString;
-            this.connection = new SqlConnection(connectionString);
+            this.connectionString = ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString;
+
+            var type = this.GetType();
+
+            PropertyInfo[] inheritorDbProperties = type.GetProperties()
+                .Where(p => p.PropertyType.Name == typeof(Repository<>).Name).ToArray();
+
+            foreach (PropertyInfo contextProperty in inheritorDbProperties) 
+            {
+                Type propertyType = contextProperty.PropertyType;          
+                var propertyCtor = propertyType.GetConstructor(
+                                BindingFlags.Instance | BindingFlags.NonPublic,
+                                binder: null, types: new Type[] { typeof(string) }, modifiers: null);
+
+                object propertyInstance = propertyCtor.Invoke(new object[] { connectionString });
+                contextProperty.SetValue(this, propertyInstance);
+            }
         }
     }
 }
