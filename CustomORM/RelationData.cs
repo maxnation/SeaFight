fba3594b1648844bbc617fa14d7fe80671725e7f
Maxim <maksim.torba@nixsolutions.com>
2020-04-12 17:02:40 +0300

    Added class Relation Data and Resource File, which contains SQL Scripts to generate stored procedure, which retrieves information about table FK constraints

---
+++ CustomORM/RelationData.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomORM
+{
+   public class RelationData
+    {      
+        public string Child_Table { get; set; }
+        public string FK_ColumnName { get; set; }
+        public string Parent_Table { get; set; }
+        public string PK_ColumnName { get; set; }
+    }
+}
