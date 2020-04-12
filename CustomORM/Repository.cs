eaa778dbfb89afeb6ff6f72923aaa5616c5132b4
Maxim <maksim.torba@nixsolutions.com>
2020-04-12 22:33:55 +0300

    Added method for loading related data - LoadRelatedData(T entity). Added overloads of method Map()

--- CustomORM/Repository.cs
+++ CustomORM/Repository.cs
@@ -15,16 +15,19 @@ namespace CustomORM
         private string insertCommandText;
         private string selectCommandText;
         private List<string> tableColumnNames;
+        private IEnumerable<RelationData> tableRelations;
 
         private Repository(string connectionString)
         {
             this.connectionString = connectionString;
             this.Initialize();
         }
+
         #region Initialization
         private void Initialize()
         {
             SetTableName();
+            SetTableRelations();
             SetCommands();
         }
         private void SetCommands()
@@ -179,6 +182,17 @@ namespace CustomORM
             }
         }
 
+        public T Find(int id, bool loadRelatedData)
+        {
+            T entity = Find(id);
+
+            if (loadRelatedData == true)
+            {
+                LoadRelatedData(entity);
+            }
+            return entity;
+        }
+
         public T Find(int id)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -198,6 +212,20 @@ namespace CustomORM
             }
         }
 
+        public IEnumerable<T> GetAll(bool loadRelatedData = false)
+        {
+            var entities = GetAll();
+            if (loadRelatedData == true)
+            {
+                foreach (var entity in entities)
+                {
+                    
+                    LoadRelatedData(entity);
+                }         
+            }
+            return entities;
+        }
+
         public IEnumerable<T> GetAll()
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -205,8 +233,6 @@ namespace CustomORM
                 SqlCommand command = new SqlCommand();
                 command.CommandText = selectCommandText;
                 command.Connection = connection;
-
-
                 connection.Open();
                 SqlDataReader reader = command.ExecuteReader();
 
@@ -251,14 +277,32 @@ namespace CustomORM
                         .First(p => (p.GetCustomAttribute(typeof(ColumnAttribute)) as ColumnAttribute)
                         ?.ColumnName == columnName);
                 }
-                property.SetValue(entity, reader[i]);
+                property?.SetValue(entity, reader[i]);
             }
 
             return (T)entity;
         }
 
+        private void Map<T>(SqlDataReader reader, T entity)
+        {
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                string columnName = reader.GetName(i);
+                var property = entity.GetType().GetProperty(columnName);
+
+                if (property == null)
+                {
+                    property = entity.GetType()
+                        .GetProperties()
+                        .First(p => (p.GetCustomAttribute(typeof(ColumnAttribute)) as ColumnAttribute)
+                        ?.ColumnName == columnName);
+                }
+                property?.SetValue(entity, reader[i]);
+            }
+        }
+
         private void SetCommandParameters(SqlCommand command, T entity)
-        {           
+        {
             foreach (var columnName in tableColumnNames)
             {
                 var property = entity.GetType().GetProperty(columnName);
@@ -276,10 +320,10 @@ namespace CustomORM
         }
         #endregion
 
-        private IEnumerable<RelationData> GetTableRelations(string tableName)
+        private void SetTableRelations()
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
-            {                
+            {
                 SqlCommand command = new SqlCommand("sp_getFkData", connection);
                 command.CommandType = System.Data.CommandType.StoredProcedure;
                 command.Parameters.AddWithValue("@tableName", tableName);
@@ -296,10 +340,95 @@ namespace CustomORM
                 }
 
                 reader.Close();
-                return relations;
+                tableRelations = relations;
             }
         }
 
+        public void LoadRelatedData(T entity)
+        {
+            object id = (entity.GetType().GetProperty("Id")?.GetValue(entity));
+            if (id == null)
+            {
+                var prop = entity.GetType()
+                  .GetProperties()
+                  .FirstOrDefault(p =>
+                  (p.GetCustomAttribute(typeof(ColumnAttribute)) as ColumnAttribute)?.ColumnName == "Id");
+
+                id = prop.GetValue(entity);
+            }
+
+            var navigationalProps = entity.GetType().GetProperties()
+                    .Where(p => p.PropertyType.GetGenericArguments().Length == 1)
+                    .ToDictionary(p => p.PropertyType.GetGenericArguments()[0].Name, p => p);
+
+            foreach (RelationData relation in tableRelations)
+            {
+                if (this.tableName == relation.Parent_Table)
+                {
+                    var navigProperty = navigationalProps[relation.Child_Table];
+                    Type navPropGenericArgType = navigProperty.PropertyType.GenericTypeArguments[0];
+
+                    var propertyCtor = navPropGenericArgType.GetConstructor(
+                                                    BindingFlags.Instance | BindingFlags.Public,
+                                                    binder: null, types: new Type[] { }, modifiers: null);
+
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        SqlCommand command = new SqlCommand();
+                        command.Connection = connection;
+                        command.CommandText = $"SELECT * FROM [{relation.Child_Table}] WHERE {relation.FK_ColumnName} = @Id";
+                        command.Parameters.AddWithValue("@Id", id);
+                        connection.Open();
+
+                        Type generic = typeof(List<>);
+                        Type[] typeArgs = { navPropGenericArgType };
+                        Type constructed = generic.MakeGenericType(typeArgs);
+                        object collection = constructed.GetConstructor(new Type[] { }).Invoke(new object[] { });
+
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                object instance = propertyCtor.Invoke(new object[] { });
+                                Map(reader, instance);
+                                constructed.GetMethod("Add").Invoke(collection, new object[] { instance });
+
+                            }
+                        }
+                        navigProperty.SetValue(entity, collection);
+                    }
+                }
+                else if (this.tableName == relation.Child_Table)
+                {
+                    var navigationalProperty = entity.GetType().GetProperty(relation.Parent_Table);
+
+                    var propertyCtor = navigationalProperty.PropertyType.GetConstructor(
+                                                    BindingFlags.Instance | BindingFlags.Public,
+                                                    binder: null, types: new Type[] { }, modifiers: null);
+
+                    object propertyInstance = propertyCtor.Invoke(new object[] { });
+
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        SqlCommand command = new SqlCommand();
+                        command.Connection = connection;
+                        command.CommandText = $"SELECT * FROM [{relation.Parent_Table}] WHERE {relation.PK_ColumnName} = @Id";
+                        command.Parameters.AddWithValue("@Id", id);
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        reader.Read();
+                        if (reader.HasRows)
+                        {
+                            Map(reader, propertyInstance);
+                        }
+                        navigationalProperty.SetValue(entity, propertyInstance);
+                    }
+                }
+            }
+        }
+     
     }
+
 }
 
